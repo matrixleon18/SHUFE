24b498ed20dee4bbaa862da9e6f49120d87c9082
root <matrixleon18@hotmail.com>
2020-04-04 17:11:09 +0800

    自回归模相关性滞后图散点图

--- ar1.py
+++ ar1.py
@@ -10,10 +10,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 hs300_data = ts.get_hist_data('399300')
-close_data = pd.Series(hs300_data['close'].to_list())
 
-pd.plotting.lag_plot(close_data)
-plt.show()
-
-# pd.plotting.autocorrelation_plot(close_data)
+# plt.plot(hs300_data['close'].to_list())                   # 原始数据的图像
 # plt.show()
+
+close_data = pd.Series(hs300_data['close'].to_list())       # 收盘价转成list,再转成series
+pd.plotting.lag_plot(close_data)                            # 绘制散点图的滞后图
+plt.show()
