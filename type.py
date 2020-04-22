8c60b1c77436b31cab4737fa896f90380e3d12e9
xiaobo <matrixleon18@hotmail.com>
2020-04-22 11:52:09 +0800

    类的使用

--- type.py
+++ type.py
@@ -1,14 +1,16 @@
-
 def init(self):
     pass
+
 
 def set_name(self, value):
     self.name = value
 
+
 def get_name(self):
     return self.name
 
-MyClass = type("TypeExample", (object, ), {"__init__":init, "setname":set_name, "getname":get_name})
+
+MyClass = type("TypeExample", (object,), {"__init__": init, "setname": set_name, "getname": get_name})
 
 p = MyClass()
 
