ecd816ba81245dae38105dd06b5aedf0e1dfcd31
root <matrixleon18@hotmail.com>
2020-04-26 12:47:41 +0800

    高级功能

--- advance/onehot.py
+++ advance/onehot.py
@@ -15,7 +15,19 @@ print(np.array(['California', 'Delaware', 'Texas'], dtype='<U10'))
 print(pd.get_dummies(x[:,0]))
 
 
+from sklearn import preprocessing
 
+enc = preprocessing.OneHotEncoder()
+
+enc.fit([[0, 0, 3],
+         [1, 1, 0],
+         [0, 2, 1],
+         [1, 0, 2]]
+        )
+
+array =enc.transform([[0,1,3]]).toarray()
+
+print(array)
 
 
 
