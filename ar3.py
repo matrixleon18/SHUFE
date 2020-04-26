a48e6b0f23eb8874d91d4c635ac0e063fba8c424
root <matrixleon18@hotmail.com>
2020-04-26 12:37:37 +0800

    自回归模型－－ARIMA模型预测

--- ar3.py
+++ ar3.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 hs300_data = ts.get_hist_data('399300')
 close_data = hs300_data['close'].to_list()
@@ -21,11 +22,17 @@ train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
 ######################################################################
-pd.plotting.autocorrelation_plot(close_data)            # 绘制自相关系数图
+pd.plotting.autocorrelation_plot(close_data)            # 用pandas绘制自相关系数图
+plt.show()
+
+plot_acf(hs300_data['close'])            # 用sm绘制自相关系数图。横坐标表示延迟阶数，纵坐标表示自相关系数。
+plt.show()
+
+plot_pacf(hs300_data['close'])            # 用sm绘制偏自相关系数图。横坐标表示延迟阶数，纵坐标表示自相关系数。
 plt.show()
 
 # fit model
-model = ARIMA(pd.Series(close_data), order=(10, 1, 0))
+model = ARIMA(pd.Series(close_data), order=(9, 1, 0))   # d=1 就是做一阶差分。（其实应该是看一阶差分后是不是平稳再定）
 model_fit = model.fit(disp=0)
 print(model_fit.summary())
 # plot residual errors
